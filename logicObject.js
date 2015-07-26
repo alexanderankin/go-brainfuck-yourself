14cc6b0622b310926ca0147e85916809eecb3476
Dave Ankin <alexanderankin@gmail.com>
2015-07-26 17:17:20 -0400

    broken commit, trying to read multiline comment

--- logicObject.js
+++ logicObject.js
@@ -1,11 +1,46 @@
 function logicObject () {
-	return 0;
+	var programFinished = false;
+	var programInfo = {};
+	
+	return (programFinished ? programInfo : programFinished);
 }
+
 logicObject.prototype = {
 	countLengthTape: function (argument) {
 		this.tapeLength = (argument || 0);
 		return this.tapeLength;
 	}
+	run: function (programString) {
+		var tape = [0],
+			tapePosition = 0,
+			l = programString.length;
+
+		function findClosing (programStringIndex, programString) {
+			var depth = 1;
+			var i;
+			for (i = programStringIndex; true; i++) { // TODO fix true to error
+				switch (programString[i]) {
+					case '[': depth++; break;
+					case ']': depth--; break;
+					default: break;
+				}
+				if (depth < 1) {return i;}
+			};
+			return i; // TODO bad program, should throw error
+		}
+
+		for (var i = 0; i < l; i++) {
+			switch (programString[i]) {
+				case '[':
+					if (tapePosition === 0) {i = findClosing(i, programString);}
+					break;
+				default: break;
+			}
+		};
+		
+		this.tapeLength = 
+	},
+
 };
 
 module.exports = logicObject;
